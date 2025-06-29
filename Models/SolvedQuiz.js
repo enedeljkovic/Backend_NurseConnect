74ca40dc77efc6f39d915b456417f5ee94afd4ef
enedeljkovic <148697260+enedeljkovic@users.noreply.github.com>
2025-06-29 12:53:20 +0200

    Add files via upload

--- Models/SolvedQuiz.js
+++ Models/SolvedQuiz.js
@@ -1,11 +1,12 @@
-
 const { DataTypes } = require('sequelize');
 const sequelize = require('./db');
+const Student = require('./student');
 
 const SolvedQuiz = sequelize.define('SolvedQuiz', {
   studentId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'studentid'
   },
   quizId: {
     type: DataTypes.INTEGER,
@@ -15,11 +16,36 @@ const SolvedQuiz = sequelize.define('SolvedQuiz', {
   correct: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'result'
   },
   total: {
     type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  solvedAt: {
+    type: DataTypes.DATE,
     allowNull: false,
+    defaultValue: DataTypes.NOW,
+    field: 'solvedat'
   }
+}, {
+  tableName: 'SolvedQuizzes',
+  timestamps: false,
+  defaultScope: {
+    attributes: {
+      exclude: ['StudentId', 'QuizId']  // makni aliasirane fk polja
+    }
+  }
+});
+
+SolvedQuiz.belongsTo(Student, {
+  foreignKey: {
+    name: 'studentid',
+    field: 'studentid'
+  },
+  targetKey: 'id',
+  as: 'student',
+  constraints: false
 });
 
 module.exports = SolvedQuiz;
